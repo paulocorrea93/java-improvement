526c8167b6bf750bff3db7c47a8c5ed92e8d4ff0
Paulo <paulocorreapc93@gmail.com>
2024-02-14 12:39:12 -0400

    operador de atribuição

--- aritimetica/aritimetica/src/Main.java
+++ aritimetica/aritimetica/src/Main.java
@@ -1,27 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5;
-        int b = 2;
+        int b = a+5;
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("a + b = " + b);
 
-        System.out.println("a + b = " + (a+b));
-        System.out.println("a - b = " + (a-b));
-        System.out.println("a x b = " + (a*b));
-        System.out.println("a / b = " + (a/b));
-        System.out.println("a % b = " + (a%b));
-        System.out.println("a++ = " + (a++));
-        System.out.println("b++ = " + (b++));
-        System.out.println("a-- = " + (a--));
-        System.out.println("b-- = " + (b--));
-        System.out.println("a+ = " + a);
-        System.out.println("a+ = " + b);
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//
+//        System.out.println("a + b = " + (a+b));
+//        System.out.println("a - b = " + (a-b));
+//        System.out.println("a x b = " + (a*b));
+//        System.out.println("a / b = " + (a/b));
+//        System.out.println("a % b = " + (a%b));
+//        System.out.println("a++ = " + (a++));
+//        System.out.println("b++ = " + (b++));
+//        System.out.println("a-- = " + (a--));
+//        System.out.println("b-- = " + (b--));
+//        System.out.println("a+ = " + a);
+//        System.out.println("a+ = " + b);
+//
+//        System.out.println("a == b: " + (a==b));
+//        System.out.println("a != b: " + (a!=b));
+//        System.out.println("a >= b: " + (a>=b));
+//        System.out.println("a <= b: " + (a<=b));
 
-        System.out.println("a == b: " + (a==b));
-        System.out.println("a != b: " + (a!=b));
-        System.out.println("a >= b: " + (a>=b));
-        System.out.println("a <= b: " + (a<=b));
+
 
     }
 }
