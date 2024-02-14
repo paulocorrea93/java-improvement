d2bc8b41ab724dc38dfc942428c17219f125dc49
Paulo <paulocorreapc93@gmail.com>
2024-02-14 12:32:35 -0400

    arquivos iniciais do estudo de JAVA

---
+++ aritimetica/aritimetica/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a x b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b++ = " + (b++));
+        System.out.println("a-- = " + (a--));
+        System.out.println("b-- = " + (b--));
+        System.out.println("a+ = " + a);
+        System.out.println("a+ = " + b);
+
+        System.out.println("a == b: " + (a==b));
+        System.out.println("a != b: " + (a!=b));
+        System.out.println("a >= b: " + (a>=b));
+        System.out.println("a <= b: " + (a<=b));
+
+    }
+}
