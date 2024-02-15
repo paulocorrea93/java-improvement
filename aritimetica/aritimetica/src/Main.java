0d0fd6c3d7dcf55e423a009d72f1fc3f5e9deffa
Paulo <paulocorreapc93@gmail.com>
2024-02-15 13:11:52 -0400

    concatenando strings

--- aritimetica/aritimetica/src/Main.java
+++ aritimetica/aritimetica/src/Main.java
@@ -3,27 +3,29 @@ public class Main {
         int a = 5;
         int b = a+5;
 
+
+
         System.out.println("a + b = " + b);
 
-//        System.out.println("a = " + a);
-//        System.out.println("b = " + b);
-//
-//        System.out.println("a + b = " + (a+b));
-//        System.out.println("a - b = " + (a-b));
-//        System.out.println("a x b = " + (a*b));
-//        System.out.println("a / b = " + (a/b));
-//        System.out.println("a % b = " + (a%b));
-//        System.out.println("a++ = " + (a++));
-//        System.out.println("b++ = " + (b++));
-//        System.out.println("a-- = " + (a--));
-//        System.out.println("b-- = " + (b--));
-//        System.out.println("a+ = " + a);
-//        System.out.println("a+ = " + b);
-//
-//        System.out.println("a == b: " + (a==b));
-//        System.out.println("a != b: " + (a!=b));
-//        System.out.println("a >= b: " + (a>=b));
-//        System.out.println("a <= b: " + (a<=b));
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a x b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b++ = " + (b++));
+        System.out.println("a-- = " + (a--));
+        System.out.println("b-- = " + (b--));
+        System.out.println("a+ = " + a);
+        System.out.println("a+ = " + b);
+
+        System.out.println("a == b: " + (a==b));
+        System.out.println("a != b: " + (a!=b));
+        System.out.println("a >= b: " + (a>=b));
+        System.out.println("a <= b: " + (a<=b));
 
 
 
---
+++ aritimetica/aritimetica/src/Main1502.java
@@ -0,0 +1,12 @@
+public class Main1502 {
+
+    public static void main(String[] args) {
+        String nome = "Paulo";
+        String sobrenome = "Correa";
+
+        System.out.println("Prof " + nome + " " + sobrenome);
+    }
+
+
+
+}
