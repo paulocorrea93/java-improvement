49ec7e0259d563be09dee2302a71c4f0a08d068f
Paulo <paulocorreapc93@gmail.com>
2024-02-16 08:04:32 -0400

    estruturas de repetição

---
+++ aritimetica/aritimetica/src/estruturasDeRepeticao.java
@@ -0,0 +1,22 @@
+public class estruturasDeRepeticao {
+
+    public static void main(String[]args){
+        for(int i = 0; i <= 5; i++){
+            System.out.println("i: " + i);
+        }
+
+        int y = 0;
+        while(y < 10){
+            System.out.println("Working");
+            y++;
+        }
+
+        int x = 0;
+
+        do{
+            System.out.println(x);
+            x++;
+        }while(x < 10);
+    }
+
+}
