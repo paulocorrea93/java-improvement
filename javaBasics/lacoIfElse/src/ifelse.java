414d9c5af2697d0a4c7e1f5cb2b317a37af46ba3
Paulo <paulocorreapc93@gmail.com>
2024-02-23 13:33:04 -0400

    exercicio if/else 2 - faixa etaria

--- javaBasics/lacoIfElse/src/ifelse.java
+++ javaBasics/lacoIfElse/src/ifelse.java
@@ -7,16 +7,33 @@ public class ifelse {
 
 //        exercício 01 - impar ou par
 
-        System.out.println("Digite um número: ");
-        int parOuImpar = tecla.nextInt();
+//        System.out.println("Digite um número: ");
+//        int parOuImpar = tecla.nextInt();
+//
+//        if(parOuImpar % 2 == 0){
+//            System.out.println(parOuImpar + " é par.");
+//        } else {
+//            System.out.println(parOuImpar + " é ímpar.");
+//        }
 
-        if(parOuImpar % 2 == 0){
-            System.out.println(parOuImpar + " é par.");
+//        exercício 02 - determinar faixa etária por idade
+
+        System.out.println("Quantos anos você tem?");
+        int idade = tecla.nextInt();
+
+        if(idade >= 0 && idade < 4){
+            System.out.println("Bebê");
+        } else if(idade >= 4 && idade < 13){
+            System.out.println("Criança.");
+        } else if(idade >= 13 && idade < 18){
+            System.out.println("Adolescente.");
+        } else if(idade >= 18 && idade < 60){
+            System.out.println("Adulto.");
+        } else if(idade > 60){
+            System.out.println("Idoso.");
         } else {
-            System.out.println(parOuImpar + " é ímpar.");
+            System.out.println("Digite uma idade válida.");
         }
-
-
 
     }
 }
