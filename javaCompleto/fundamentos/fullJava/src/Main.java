2458917c440786bc0ad87209e4ebcc2491ee4280
Paulo <paulocorreapc93@gmail.com>
2024-02-26 10:59:51 -0400

    exercício 02 - qual dos dois números é o maior

--- javaCompleto/fundamentos/fullJava/src/Main.java
+++ javaCompleto/fundamentos/fullJava/src/Main.java
@@ -12,10 +12,32 @@ public class Main {
         System.out.println("Informe a sua idade:");
         int idade = tecla.nextInt();
 
-        if(idade >= 18){
+        if (idade >= 18) {
             System.out.println("Maior de idade.");
         } else {
             System.out.println("Menor de idade.");
+        }
+
+        //        Escreva um programa que avalie duas variáveis inteiras, a e b. Se a for maior que b,
+//        imprima "A é maior". Se b for maior que a, imprima "B é maior". Se ambos forem iguais,
+//        imprima "A e B são iguais".
+
+
+        System.out.println("Digite um número: ");
+        int a = tecla.nextInt();
+
+        System.out.println("Digite outro número: ");
+        int b = tecla.nextInt();
+
+        if(a > b){
+            System.out.println(a + " é maior que " + b);
+            System.out.println("a é maior que b");
+        } else if (b > a){
+            System.out.println(b + " é maior que " + a);
+            System.out.println("b é maior que a");
+        } else {
+            System.out.println(a + " e " + b + " são iguais");
+            System.out.println("a e b são iguais");
         }
 
     }
