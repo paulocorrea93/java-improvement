4dfbce7ed93b988065bde02d574281e4bb5f4519
Paulo <paulocorreapc93@gmail.com>
2024-02-26 11:07:04 -0400

    exercício 03 - dia da semana

--- javaCompleto/fundamentos/fullJava/src/Main.java
+++ javaCompleto/fundamentos/fullJava/src/Main.java
@@ -5,40 +5,65 @@ public class Main {
 
         Scanner tecla = new Scanner(System.in);
 
-//        Dada uma variável idade, crie uma estrutura de controle de fluxo
-//        que imprima "Maior de idade" se a idade for igual ou superior a 18,
-//        e "Menor de idade" caso contrário.
+//        Dada uma variável idade, crie uma estrutura de controle de fluxo que imprima "Maior de idade" se a idade for
+//        igual ou superior a 18, e "Menor de idade" caso contrário.
 //
-        System.out.println("Informe a sua idade:");
-        int idade = tecla.nextInt();
+//        System.out.println("Informe a sua idade:");
+//        int idade = tecla.nextInt();
+//
+//        if (idade >= 18) {
+//            System.out.println("Maior de idade.");
+//        } else {
+//            System.out.println("Menor de idade.");
+//        }
 
-        if (idade >= 18) {
-            System.out.println("Maior de idade.");
-        } else {
-            System.out.println("Menor de idade.");
+//        Escreva um programa que avalie duas variáveis inteiras, a e b.Se a for maior que b, imprima "A é maior".Se b
+//        for maior que a, imprima "B é maior". Se ambos forem iguais, imprima "A e B são iguais".
+
+
+//        System.out.println("Digite um número: ");
+//        int a = tecla.nextInt();
+//
+//        System.out.println("Digite outro número: ");
+//        int b = tecla.nextInt();
+//
+//        if(a > b){
+//            System.out.println(a + " é maior que " + b);
+//            System.out.println("a é maior que b");
+//        } else if (b > a){
+//            System.out.println(b + " é maior que " + a);
+//            System.out.println("b é maior que a");
+//        } else {
+//            System.out.println(a + " e " + b + " são iguais");
+//            System.out.println("a e b são iguais");
+//        }
+
+
+//        Implemente um programa que use a estrutura switch para lidar com o dia da semana. Dada uma variável dia
+//        contendo um valor de 1 a 7 (onde 1 é domingo e 7 é sábado), imprima o nome do dia correspondente.
+
+
+        System.out.println("Digite o número do dia da semana:");
+        int dia = tecla.nextInt();
+
+        switch (dia){
+            case 1 : System.out.println("Doming");
+            break;
+            case 2 : System.out.println("Segunda");
+            break;
+            case 3 : System.out.println("Terça");
+            break;
+            case 4 : System.out.println("Quarta");
+            break;
+            case 5 : System.out.println("Quinta");
+            break;
+            case 6 : System.out.println("Sexta");
+            break;
+            case 7 : System.out.println("Sábado");
+            break;
+            default : System.out.println("Digite um núemro de 1 a 7");
         }
 
-        //        Escreva um programa que avalie duas variáveis inteiras, a e b. Se a for maior que b,
-//        imprima "A é maior". Se b for maior que a, imprima "B é maior". Se ambos forem iguais,
-//        imprima "A e B são iguais".
-
-
-        System.out.println("Digite um número: ");
-        int a = tecla.nextInt();
-
-        System.out.println("Digite outro número: ");
-        int b = tecla.nextInt();
-
-        if(a > b){
-            System.out.println(a + " é maior que " + b);
-            System.out.println("a é maior que b");
-        } else if (b > a){
-            System.out.println(b + " é maior que " + a);
-            System.out.println("b é maior que a");
-        } else {
-            System.out.println(a + " e " + b + " são iguais");
-            System.out.println("a e b são iguais");
-        }
 
     }
 }
